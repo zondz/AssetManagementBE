cd28705d0d4e7e8038947b1c57b583c76c582a36
Hai Duong <106789880+duongyn@users.noreply.github.com>
2022-07-24 14:10:52 +0700

    Create User BE - Update Max length validation

---
+++ asset management/src/main/java/com/nt/rookies/asset/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package com.nt.rookies.asset.dto;
+
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+	private String status;
+
+	@NotBlank(message = "First name can not be empty")
+	@Length(min = 2, max = 10, message = "First name is not valid! Please input first name with length from 2 to 10 letters")
+	@Pattern(regexp = "[a-zA-Z]+", message = "First name must not contain numbers, whitespace and special characters")
+	private String firstName;
+
+	@NotBlank(message = "Last name can not be empty")
+	@Length(min = 2, max = 20, message = "Last name is not valid! Please input last name with length from 2 to 20 letters")
+	@Pattern(regexp = "[a-zA-Z\\s]+", message = "Last name must not contain numbers and special characters")
+	private String lastName;
+
+	private String username;
+
+	@NotBlank(message = "Date of birth can not be empty")
+	private String dob;
+
+	@NotBlank(message = "Joined date can not be empty")
+	private String joinedDate;
+
+	private String gender;
+
+	@NotBlank(message = "Type can not be empty")
+	private String type;
+
+	private String location;
+
+	private boolean isDeleted;
+
+	private boolean isFirstLogin;
+}
---
+++ asset management/src/main/java/com/nt/rookies/asset/entities/CategoryEntity.java
@@ -0,0 +1,36 @@
+package com.nt.rookies.asset.entities;
+
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category", uniqueConstraints = {
+		@UniqueConstraint(columnNames = "category_name"),
+		@UniqueConstraint(columnNames = "prefix")
+})
+@Getter
+@Setter
+public class CategoryEntity extends BaseEntity {
+	@Id
+	@Column(name = "id")
+	private Integer categoryId;
+
+	@Column(name = "category_name")
+	private String categoryName;
+
+	@Column(name = "prefix")
+	private String prefix;
+
+	@OneToMany(mappedBy = "category")
+	private List<AssetEntity> assets;
+}
