21a900085742979b62339bc083618d4c92bd7f22
Hai Duong <106789880+duongyn@users.noreply.github.com>
2022-07-25 09:27:11 +0700

    Update AssetCodeGenerator

--- asset management/src/main/java/com/nt/rookies/asset/dto/UserDTO.java
+++ asset management/src/main/java/com/nt/rookies/asset/dto/UserDTO.java
@@ -2,6 +2,7 @@ package com.nt.rookies.asset.dto;
 
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import lombok.Data;
--- asset management/src/main/java/com/nt/rookies/asset/utils/AssetCodeGenerator.java
+++ asset management/src/main/java/com/nt/rookies/asset/utils/AssetCodeGenerator.java
@@ -4,11 +4,11 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 import java.util.Properties;
 
 import com.nt.rookies.asset.entities.AssetEntity;
-import lombok.Getter;
-import lombok.Setter;
+import com.nt.rookies.asset.entities.CategoryEntity;
 import org.hibernate.HibernateException;
 import org.hibernate.MappingException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
@@ -17,8 +17,6 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.type.Type;
 
 
-@Getter
-@Setter
 public class AssetCodeGenerator implements IdentifierGenerator {
 
 	private Integer maxId = 0;
@@ -32,38 +30,15 @@ public class AssetCodeGenerator implements IdentifierGenerator {
 	public Serializable generate(SharedSessionContractImplementor session, Object obj)
 			throws HibernateException {
 
-		Connection connection = session.connection();
 		AssetEntity asset = (AssetEntity) obj;
-		try {
-			PreparedStatement ps = connection.prepareStatement("select * from asset a where a.code like '"
-					+ asset.getCategory().getPrefix() + "%' order by a.code desc limit 1");
-			ResultSet rs = ps.executeQuery();
+		String query = "from AssetEntity a where a.assetCode like '" +
+				asset.getCategory().getPrefix() + "%' order by a.assetCode desc";
+		List<AssetEntity> list = session.createQuery(query).setMaxResults(1).getResultList();
 
-			boolean flag = false;
+		Integer maxId = list.stream().map(a -> String.valueOf(a.getAssetCode())
+						.replace(asset.getCategory().getPrefix(), ""))
+				.mapToInt(a -> Integer.parseInt(a)).max().orElse(0);
 
-			if (rs.next()) {
-				String numberPart = rs.getString("code")
-						.substring(String.valueOf(asset.getCategory().getPrefix()).length());
-				try {
-					Integer numPart = Integer.parseInt(numberPart);
-					this.setMaxId(numPart);
-					flag = true;
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			if (!flag) {
-				this.setMaxId(1);
-			}
-			else {
-				this.setMaxId((this.getMaxId() + 1));
-			}
-			connection.close();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		return asset.getCategory().getCategoryId() + String.format("%06d", this.getMaxId());
+		return asset.getCategory().getPrefix() +  String.format("%06d", (maxId+1));
 	}
 }
